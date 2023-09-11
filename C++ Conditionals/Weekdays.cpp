004ea8c01c1276f8f37e4fc0dad28c31dc4fc326
Mrityunjoy Kundu <bubunkundu170@gmail.com>
2023-09-11 14:48:18 +0530

    Weekdays

---
+++ C++ Conditionals/Weekdays.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int Day_No;
+    cin>>Day_No;
+
+    switch (Day_No)
+    {
+    case 1: 
+    cout<<"Sunday"<<endl;
+        break;
+    case 2: 
+    cout<<"Monday"<<endl;
+        break;
+    case 3: 
+    cout<<"Tuesday"<<endl;
+        break;
+    case 4: 
+    cout<<"Wednesday"<<endl;
+        break;
+    case 5: 
+    cout<<"Thursday"<<endl;
+        break;
+    case 6: 
+    cout<<"Friday"<<endl;
+        break;
+    case 7: 
+    cout<<"Saturday"<<endl;
+        break;
+    default:
+    cout<<"Wrong choice"<<endl;
+    }
+
+return 0;
+}
